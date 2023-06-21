1fa212cd24e57188f3d6d88e0d24a22253fb0353
noah3-0 <prowazix@gmail.com>
2023-06-21 08:45:51 +0200

    add close button, css html only

--- index.js
+++ index.js
@@ -463,10 +463,17 @@ submitPollBtn.addEventListener("submit", async (e) => {
       const newPollHtml = `
           <div class="poll-module">
           <div class="poll-number">${numberPollCount + 1}</div>
+          <div class="close-button">&#10006;</div>
           <h2 class="poll-title">${data.question}</h2>
               <div class="poll-options">
                   <button class="poll-option">Yes</button>
                   <button class="poll-option">No</button>
+              </div>
+              <div class="close-button">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <line x1="18" y1="6" x2="6" y2="18"></line>
+                <line x1="6" y1="6" x2="18" y2="18"></line>
+              </svg>
               </div>
           </div>
         `;
@@ -529,11 +536,18 @@ async function updatePoll() {
       pollBox.innerHTML += `
           <div class="poll-module">
           <div class="poll-number">${i + 1}</div>
+          <div class="close-button">&#10006;</div>
           <h2 class="poll-title">${data.question}</h2>
                   <div class="poll-options">
                       <button class="poll-option" data-poll-index="${i}" data-vote="true">Yes</button>
                       <button class="poll-option" data-poll-index="${i}" data-vote="false">No</button>
                   </div>
+                  <div class="close-button">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                      <line x1="18" y1="6" x2="6" y2="18"></line>
+                      <line x1="6" y1="6" x2="18" y2="18"></line>
+                    </svg>
+                    </div>
           </div>
           `;
 
